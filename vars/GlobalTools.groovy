d6ee7406d2811333c3dd56a0d92b70af8301eff0
linjianshan <1355969979@qq.com>
2022-12-09 01:34:06 +0800

    add vars test methons
    Signed-off-by: linjianshan <1355969979@qq.com>

---
+++ vars/GlobalTools.groovy
@@ -0,0 +1,18 @@
+import org.devops.*;
+
+//def call(){
+//    println helle nihao i am vars
+//}
+
+
+/**
+ * 字符串大小写转换
+ * @param str
+ * @return
+ */
+def toLowerCase(String str) {
+    def tools = new tools();
+
+    return tools.toLowerCase(str);
+
+}
